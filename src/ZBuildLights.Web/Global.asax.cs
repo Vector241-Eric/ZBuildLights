9ecfbf86b21f891ba7ee59e400b7a03c7df0ce0f
Vector241-Eric <eric@vector241.com>
2015-04-12 14:40:37 -0500

    Setup NLog

--- src/ZBuildLights.Web/Global.asax.cs
+++ src/ZBuildLights.Web/Global.asax.cs
@@ -1,15 +1,22 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 using ZBuildLights.Core.Services;
 
 namespace ZBuildLights.Web
 {
     public class MvcApplication : HttpApplication
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private static readonly Guid ApplicationId = Guid.NewGuid();
+
         protected void Application_Start()
         {
+            Log.Debug("Application ({0}) starting", ApplicationId);
+
             AreaRegistration.RegisterAllAreas();
             InitializeStaticFactories();
             AutoMapperConfig.Initialize();
@@ -21,6 +28,11 @@ namespace ZBuildLights.Web
         private void InitializeStaticFactories()
         {
             SystemClock.UseCurrentTime();
+        }
+
+        protected void Application_End()
+        {
+            Log.Debug("Application ({0}) ending", ApplicationId);
         }
     }
 }
