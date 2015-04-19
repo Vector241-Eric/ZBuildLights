6b8a9b4bbcb7f2f856e2da82000eda85e899ff5e
Vector241-Eric <eric@vector241.com>
2015-04-19 18:40:44 -0500

    Add a test for CcReader

---
+++ src/IntegrationTests/ZBuildLights.Core/Services/CruiseControl/CcReaderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Should;
+using ZBuildLights.Core.Services.CruiseControl;
+
+namespace IntegrationTests.ZBuildLights.Core.Services.CruiseControl
+{
+    public class CcReaderTests
+    {
+        [TestFixture]
+        public class When_happy_path
+        {
+            [Test, Explicit]
+            public void Should_read_ccXml()
+            {
+                var reader = new CcReader();
+                var result =
+                    reader.GetStatus(
+                        "http://csbeap-ci1.oldev.preol.dell.com:8080/guestAuth/app/rest/cctray/projects.xml");
+                result.IsSuccessful.ShouldBeTrue();
+                Console.WriteLine(JsonConvert.SerializeObject(result.Data, Formatting.Indented));
+            } 
+        } 
+    }
+}
