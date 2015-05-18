f42cde67edecab5105c4b194e342e476c06a0904
Vector241-Eric <eric@vector241.com>
2015-05-18 01:01:34 -0500

    Remove dead code

--- src/SolutionScripts/01_LocalDeploymentSettings.ps1
+++ src/SolutionScripts/01_LocalDeploymentSettings.ps1
@@ -3,7 +3,6 @@ $global:localDeploymentSettings = @{}
 $localDeploymentSettings.DeploymentPath = "c:\inetpub\ZBuildLights"
 $localDeploymentSettings.AppPool = "ZBuildLights"
 $localDeploymentSettings.WebSiteName = "ZBuildLights"
-$localDeploymentSettings.WebApplicationName = "ZBuildLights"
 $localDeploymentSettings.Port = "8088"
 $localDeploymentSettings.Project = "ZBuildLights.Web"
 $localDeploymentSettings.BuildConfiguration = "Release"
